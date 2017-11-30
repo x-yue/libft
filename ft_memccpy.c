5179222ea23c676039d5d645508ee3fe3156949a
yuxu <yuxu@github.com>
2017-11-30 22:51:21 +0100

    tests

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: yuxu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 19:06:35 by yuxu              #+#    #+#             */
-/*   Updated: 2017/11/28 22:00:04 by yuxu             ###   ########.fr       */
+/*   Updated: 2017/11/30 22:50:42 by yuxu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,26 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char	*dst1;
-	unsigned char	*src1;
-	int				i;
+	unsigned char		*dst1;
+	unsigned const char	*src1;
+	int					i;
 
-	dst1 = (unsigned char*)dst;
-	src1 = (unsigned char*)src;
+	dst1 = dst;
+	src1 = src;
 	i = 0;
-	if (n < 0 && )
-		
-	if (n > 0)
+	while (n--)
 	{
-		while (src1[i] && n--)
-		{
-			i++;
-			if (src1[i] == (unsigned char)c)
-				return (dst1 + i + 1);
-		}
+		dst1[i] = src1[i];
+		i++;
 	}
+	if (dst1[i] == (unsigned char)c)
+		return ((unsigned char*)dst1 + 1);
 	return (NULL);
 }
 
 int		main(int ac, char **av)
 {
-	printf("%s\n", memccpy((char*)av[1], (char*)av[2],(char)av[3][0], atoi(av[4])));
+	printf("%s\n", memccpy((char*)av[1], (char*)av[2],av[3][0], atoi(av[4])));
 	printf("%s\n",ft_memccpy((char*)av[1], (char*)av[2],(char)av[3][0], atoi(av[4])));
 	return 0;
 }
